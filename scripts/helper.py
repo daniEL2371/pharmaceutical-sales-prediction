6843f002d83f52fc3d930e574cf4c27548b9515d
Daniel <danielzelalemheru@gmail.com>
2021-07-26 12:56:25 +0300

    feat: add helper and logger modules

---
+++ scripts/helper.py
@@ -0,0 +1,88 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import Normalizer, MinMaxScaler, StandardScaler
+import pickle
+import dvc.api
+from app_logger import App_Logger
+
+from app_logger import App_Logger
+helper = Helper()
+
+
+app_logger = App_Logger("helper.log").get_app_logger()
+
+
+class Helper:
+
+    def __init__(self):
+        self.logger = App_Logger("helper.log").get_app_logger()
+
+    def read_model(self, file_name):
+        with open(f"../models/{file_name}.pkl", "rb") as f:
+            self.logger.info(f"Model loaded from {file_name}.pkl")
+            return pickle.load(f)
+
+    def write_model(self, file_name, model):
+        with open(f"../models/{file_name}.pkl", "wb") as f:
+            self.logger.info(f"Model dumped to {file_name}.pkl")
+            pickle.dump(model, f)
+
+    def read_csv(self, csv_path, missing_values=[]):
+        try:
+            df = pd.read_csv(csv_path, na_values=missing_values)
+            print("file read as csv")
+            self.logger.info(f"file read as csv from {csv_path}")
+            return df
+        except FileNotFoundError:
+            print("file not found")
+            self.logger.error(f"file not found, path:{csv_path}")
+
+    def save_csv(self, df, csv_path):
+        try:
+            df.to_csv(csv_path, index=False)
+            print('File Successfully Saved.!!!')
+            self.logger.info(f"File Successfully Saved to {csv_path}")
+
+        except Exception:
+            print("Save failed...")
+            self.logger.error(f"saving failed")
+
+        return df
+
+    def get_data(tag, path='Data/clean_data.csv', repo='https://github.com/daniEL2371/pharmaceutical-sales-prediction'):
+        rev = tag
+        data_url = dvc.api.get_url(path=path, repo=repo, rev=rev)
+        df = pd.read_csv(data_url)
+        app_logger.info(f"Read data from {path}, version {tag}")
+
+        return df
+
+    def percent_missing(self, df: pd.DataFrame) -> float:
+
+        totalCells = np.product(df.shape)
+        missingCount = df.isnull().sum()
+        totalMissing = missingCount.sum()
+        return round((totalMissing / totalCells) * 100, 2)
+
+    def percent_missing_for_col(self, df: pd.DataFrame, col_name: str) -> float:
+        total_count = len(df[col_name])
+        if total_count <= 0:
+            return 0.0
+        missing_count = df[col_name].isnull().sum()
+
+        return round((missing_count / total_count) * 100, 2)
+
+    def normalizer(self, df, columns):
+        norm = Normalizer()
+        return pd.DataFrame(norm.fit_transform(df), columns=columns)
+
+    def scaler(self, df, columns, mode="minmax"):
+        if (mode == "minmax"):
+            minmax_scaler = MinMaxScaler()
+            return pd.DataFrame(minmax_scaler.fit_transform(df), columns=columns)
+        elif (mode == "standard"):
+            scaler = StandardScaler()
+            return pd.DataFrame(scaler.fit_transform(df), columns=columns)
+
+    def scale_and_normalize(self, df, columns, sclaer_mode="minmax"):
+        return self.normalizer(self.scaler(df, columns, sclaer_mode), columns)
